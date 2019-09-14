6a4eed906bd519a563ab4d00e5a96f17ac0bb777
Nathan Au <nathan.au1280@gmail.com>
2019-09-14 13:20:47 -0400

    Updated camera permissions and allowed manipulation of captured photo

--- app/src/main/java/com/example/asl_ml/MainActivity.java
+++ app/src/main/java/com/example/asl_ml/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.asl_ml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        dispatchTakePictureIntent();
+
+
+        //imageView.setImageDrawable()
+
+
+
         setContentView(R.layout.activity_main);
+
+
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView = (ImageView) findViewById(R.id.cameraDisplay);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
 }
